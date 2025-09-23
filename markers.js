34a7972a1f279616b9f5b637f983de47c4561bbc
Grunn-Yo <manoytrip@gmail.com>
2025-09-23 23:03:03 +0200

    Create markers.js

---
+++ markers.js
@@ -0,0 +1,142 @@
+// lijst met markers
+addMarker(1, 1, 53.205113, 6.57997737, "Voormalig Kempkensberg(?)");
+addMarker(1, 2, 53.21844, 6.56627, "Borstbeeld Carl Rabenhaupt");
+addMarker(1, 3, 53.21829, 6.56816, "Café De Groote Griet <br/> (Vernoemd naar Rabenhaupt's kanon)");
+addMarker(1, 4, 53.2157232, 6.5699404, "Café 1672 <br/> Kanonskogels in voorgevel");
+
+addMarker(2, 1, 53.199923, 6.581904, "Zernike College <br/> voormalig Dalton HBS");
+
+addMarker(3, 1, 53.22187252, 6.5627871, "Parkeergarage Ossenmarkt");
+addMarker(3, 2, 53.22222597705039, 6.562275774201809, "Sichtermanhuis anno 1745");
+addMarker(3, 3, 53.22184905627311, 6.563550146962064, "Voormalige woning W.F. Hermans");
+
+addMarker(4, 1, 53.21789912507943, 6.556084405222586, "Natuurkundig Laboratorium");
+addMarker(4, 2, 53.2177047022648, 6.555008169042122, "Het Kasteel <br/> Mineralogisch-Geologisch Instituut");
+addMarker(4, 3, 53.217214774891765, 6.5548483930653285, "Melkweg");
+
+addMarker(5, 1, 53.219766038766984, 6.556883083156669, "Voormalig Stoomzuivelfabriek en Melkinrichting 'Stad en Lande'");
+addMarker(5, 2, 53.230331064070455, 6.532055257526586, "Voormalig melkfabriek 'De Ommelanden'");
+
+addMarker(6, 1, 53.213238165577614, 6.561308609175683, "Voormalig zusterhuis");
+addMarker(6, 2, 53.21254827637292, 6.563800458150554, "Voormalig kraamkliniek Diakonessenhuis");
+
+addMarker(7, 1, 53.227743093005934, 6.567729690207255, "Nijverheidsschool (MTS)");
+addMarker(7, 2, 53.21487539397596, 6.561484619947047, "Academie Minerva");
+
+addMarker(8, 1, 53.21476154595684, 6.49848066467272, "Noord-Nederlandse Beetwortelsuikerfabriek");
+addMarker(8, 2, 53.21042275496406, 6.537248696812011, "Friesch-Groningsche Coöperatieve Beetwortelsuikerfabriek");
+
+addMarker(9, 1, 53.22365089656641, 6.5688766788199455, "Voormalige gemeentelijke electriciteitscentrale");
+addMarker(9, 2, 53.20413739089533, 6.589665412334242, "Voormalige centrale Helpman <br/> provinciale electriciteitscentrale");
+addMarker(9, 3, 53.208542693337954, 6.589902186221441, "Voormalige Hunzecentrale");
+
+addMarker(10, 1, 53.200906428680725, 6.577780150640764, "Natte brug over het Helperdiepje");
+addMarker(10, 2, 53.20245680864304, 6.577361393153131, "FPC Dr. S. van Mesdag (tbs-kliniek) <br/> Voormalige Cellulaire Strafgevangenis");
+addMarker(10, 3, 53.20385277314929, 6.580574524420053, "Locatie Engelse Kamp <br/> Interneringskamp Britse militairen WOI");
+addMarker(10, 4, 53.201965662955594, 6.57502530448252, "Bunker (koude oorlog)");
+
+addMarker(11, 1, 53.206837659945286, 6.567005122117461, "Mariënholm, voormalig asielzoekerscentrum");
+addMarker(11, 2, 53.209701447494794, 6.571412714512196, "Rabenhauptstraat");
+addMarker(11, 3, 53.21571509646478, 6.567058311920908, "Pelstergasthuiskerk <br/> Église Wallonne");
+addMarker(11, 4, 53.19552859127521, 6.579792547963918, "Locatie Babyhuis Prinses Margriet <br/> Opvang ondervoede baby's WOII");
+
+addMarker(12, 1, 53.211472051522534, 6.557343916197458, "Hoofdgebouw voormalig gemeentelijk badhuis <br/> 1881 - 1955");
+addMarker(12, 2, 53.194793733286744, 6.578911545119826, "Helperbad <br/> sinds 1925");
+
+addMarker(13, 1, 53.2156, 6.57085, "Volksgaarkeuken en kosthuizen");
+addMarker(13, 2, 53.21535, 6.57047, "Nieuwbouw Zuiderdiep 1998");
+
+addMarker(14, 1, 53.21486437, 6.572872006, "Sint Jozefkathedraal");
+
+addMarker(15, 1, 53.21725, 6.57169, "Pepergasthuis");
+addMarker(15, 2, 53.21816, 6.56438, "Soephuisstraatje <br/> Commissie van Spijsuitdeling");
+addMarker(15, 3, 53.21723, 6.57828, "Tehuis voor dakloozen");
+
+addMarker(16, 1, 53.2190, 6.5661, "Noordzijde Grote Markt");
+
+addMarker(17, 1, 53.22173726280241, 6.5552532322227925, "Sikkenspoortje");
+
+addMarker(18, 1, 53.204066, 6.5538873, "Voormalige locatie ijsbaan");
+addMarker(18, 2, 53.237677, 6.5964882, "IJsbaan Kardinge");
+
+addMarker(19, 1, 53.210941, 6.564069, "Hoofdstation Groningen");
+
+addMarker(20, 1, 53.219156, 6.55806156, "Voormalige zone raamprostitutie");
+addMarker(20, 2, 53.21509, 6.56553, "Nieuwstad <br/> Zone raamprostitutie");
+addMarker(20, 3, 53.208595, 6.59624, "Voormalige tippelzone Bornholmstraat");
+
+addMarker(21, 1, 53.216737, 6.5634036, "Korenbeurs");
+
+addMarker(22, 1, 53.218549697934435, 6.566947064061545, "Voormalige locatie politiebureau <br/> tot 1905");
+addMarker(22, 2, 53.21146893647456, 6.573840548845822, "Voormalig hulpbureau politie <br/> 1875-1966");
+addMarker(22, 3, 53.21976869977467, 6.5679878122798465, "Voormalige locatie politiebureau <br/> 1905-1971");
+addMarker(22, 4, 53.22646135161808, 6.556568326727859, "Voormalig hulpbureau politie <br/> 1903-1963");
+addMarker(22, 5, 53.21602821630878, 6.5556535327048495, "Voormalig hulpbureau politie <br/> 1920s-1963");
+addMarker(22, 6, 53.21388931414113, 6.565461836180347, "Voormalig hulpbureau politie <br/> 1928-1951");
+addMarker(22, 7, 53.19829866109128, 6.579628248208602, "Voormalig hulpbureau politie <br/> 1928(?)-1966");
+addMarker(22, 8, 53.22199733110715, 6.584171679725236, "Voormalig hulpbureau politie <br/> 1931(?)-1966");
+addMarker(22, 9, 53.2154808046047, 6.572413694180369, "Politiebureau <br/> sinds 1972");
+
+addMarker(23, 1, 53.1255133286087, 6.581842023081133, "Vliegveld Eelde");
+
+addMarker(24, 1, 53.20497563143166, 6.574250738733696, "Zuidelijke ringweg");
+
+addMarker(26, 1, 53.22400509595307, 6.567659837747023, "Gebouw oude gasfabriek");
+addMarker(26, 2, 53.22431318242749, 6.565714476080761, "Bedrijfshal oude gasfabriek");
+addMarker(26, 3, 53.22483866532292, 6.564387032299158, "Pijp oude gasfabriek");
+addMarker(26, 4, 53.225957135658746, 6.569374823727367, "Voormalig bodenterrein");
+addMarker(26, 5, 53.225789706341665, 6.563424995749048, "De jodenkamp <br/> Voormalige joodse begraafplaats");
+
+addMarker(27, 1, 53.21884438451105, 6.557939313438112, "Vaatje Brouwerij De Sleutel");
+
+addMarker(28, 1, 53.240033968718635, 6.576037319549974, "Oude loop rivier Hunze");
+addMarker(28, 2, 53.211010154908635, 6.610106685000568, "Hunzeloop");
+addMarker(28, 3, 53.21337779946002, 6.543887542847564, "Hoendiep");
+addMarker(28, 4, 53.220682959893765, 6.584047299639567, "Voormalige loop Damsterdiep");
+addMarker(28, 5, 53.21661900411652, 6.559345385149652, "A-brug <br/> Eerste draaibare brug in Diepenring");
+addMarker(28, 6, 53.2216502879763, 6.563981948438077, "Spilsluizen <br/> Voormalige sluis ivm getijden Reitdiep");
+addMarker(28, 7, 53.21514712642895, 6.577775667552707, "Bontebrug");
+addMarker(28, 8, 53.21504720538076, 6.557007505650942, "Voormalige Westerhaven");
+
+addMarker(29, 1, 53.22335863698571, 6.558859285655674, "Locatie Groote Schouwburg <br/> 1798-1883");
+addMarker(29, 2, 53.22087049243402, 6.572069466435375, "Stadsschouwburg");
+addMarker(29, 3, 53.21905855802952, 6.561716247538766, "Locatie cultureel complex De Harmonie");
+
+addMarker(30, 1, 53.217929281918515, 6.5705846325242385, "Jazzcafé de Spieghel");
+
+addMarker(31, 1, 53.22343856102672, 6.56280023364719, "1e Fongers Rijwielenfabriek");
+addMarker(31, 2, 53.2073715614918, 6.5728019595609695, "2e Fongers Rijwielenfabriek");
+
+addMarker(33, 1, 53.23927549296387, 6.534625713294005, "WSN-Gebouw");
+
+addMarker(34, 1, 53.22835781143251, 6.565458822247965, "Woonblok Bernoulliplein <br/> Ontworpen door Jo Boer");
+addMarker(34, 2, 53.22988384867348, 6.574480338132674, "Noorderbad <br/> Ontworpen door Jo Boer");
+addMarker(34, 3, 53.02705480345685, 5.6739489005230785, "KING-fabriek <br/> Ontworpen door Jo Boer");
+
+addMarker(35, 1, 53.22272272156979, 6.557841289598511, "Oude Hortus Botanicus");
+
+addMarker(37, 1, 53.24462963362889, 6.547089413882324, "Locatie Kasteel Selwerd");
+addMarker(37, 2, 53.24503641344943, 6.530569956731923, "Locatie Tweede Kasteel Selwerd");
+addMarker(37, 3, 53.23959507747501, 5.99064884359942, "Schierstins Feanwâlden");
+
+addMarker(39, 1, 53.20203634669874, 6.576153530149379, "Borstbeeld Majoor Lodewijk Thomson");
+addMarker(39, 2, 53.20602906781497, 6.571688753413007, "Zuiderbegraafplaats met graf L. Thomson");
+
+addMarker(40, 1, 53.22491479087641, 6.5850036959207685, "Het Witte dorp <br/> Nieuwbouw arbeiderswoningen 1925 (Eltjo Rugge)");
+addMarker(40, 2, 53.22311557130082, 6.586360320191203, "Het Oranje dorp <br/> Nieuwbouw arbeiderswoningen 1925 (Eltjo Rugge)");
+addMarker(40, 3, 53.22716929538804, 6.579921704612867, "Locatie voormalig Oosterpark (voetbalstadion)");
+addMarker(40, 4, 53.21151262251437, 6.575841917868849, "Locatie voormalig gemeentelijk bad- en waschhuis");
+addMarker(40, 5, 53.22941527749215, 6.581187941357561, "Oosterpark, Monument Eltjo Rugge");
+
+addMarker(41, 1, 53.219148765763386, 6.569340848952082, "'t Feithhuis");
+addMarker(41, 2, 53.21508108893028, 6.559754905281157, "Groningsch Museum");
+
+addMarker(42, 1, 53.07563295945945, 6.85989692150328, "Margaretha Hardenbergkerk <br/> Begraafplaats A.G. Wildervank");
+
+addMarker(43, 1, 53.21647067922779, 6.565259572523584, "Woning Truda Clant");
+addMarker(43, 2, 53.27073385977089, 6.289840007600494, "Locatie Aykemaborg");
+
+addMarker(44, 1, 53.21772243391641, 6.574025093662375, "Biljartfabriek Bierling-de Schepper");
+
+addMarker(45, 1, 53.18602490841084, 6.590797877868495, "Stadion Esserberg Be Quick");
+addMarker(45, 2, 53.222168607549015, 6.568721813791477, "Architectenbureau Evert van Linge");
