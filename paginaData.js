91925d6f51690da4c6738b9ab32bc5567be04928
ManoyTrip <manoy.trip@hotmail.com>
2026-02-17 00:06:44 +0100

    typo + additional data

--- paginaData.js
+++ paginaData.js
@@ -4,4 +4,23 @@ const afleveringenData = {
     ondertitel: "Het verhaal van het beleg van Groningen",
     url: "https://www.youtube.com/watch?v=OMLHA_iOXbo"
   }
+ 
+  2: {
+    titel: "Jacques Wallage",
+    ondertitel: "Burgemeester van Groningen",
+    url: "https://www.youtube.com/watch?v=56Cz5rjDwwI"
+  }
+
+  3: {
+    titel: "De Ossenmarkt",
+    ondertitel: "De bouw van een parkeergarage",
+    url: "https://www.youtube.com/watch?v=S8G33Vkq1cc"
+  }
+
+  4: {
+    titel: "Het Kasteel / Noorderpoortcollege",
+    ondertitel: "Een nieuw schoolgebouw",
+    url: "https://www.youtube.com/watch?v=dbhCEHQksvw"
+  }
+
 };
