220f0e1b7c2b3ba98c38e60d78d5516557651bad
ManoyTrip <manoy.trip@hotmail.com>
2026-02-16 23:57:27 +0100

    add paginaData

---
+++ paginaData.js
@@ -0,0 +1,7 @@
+const afleveringenData = {
+  1: {
+    titel: "Bommen Berend",
+    ondertitel: "Het verhaal van het beleg van Groningen",
+    url: "https://www.youtube.com/watch?v=OMLHA_iOXbo"
+  }
+};
